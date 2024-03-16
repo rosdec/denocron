105038c5d28195d912237d72159fb760645a67e9
Rosario De Chiara <rosario.dechiara@posteitaliane.it>
2024-03-16 16:12:16 +0100

    ** fix ** fixed typos in the messages

--- example1.ts
+++ example1.ts
@@ -1,7 +1,6 @@
-Deno.cron("string", "* * * * *", 
-    () => console.log("This will print every ONE minutes"))
+Deno.cron("task1", "* * * * *", 
+    () => console.log("This will print every ONE minute"))
 
 
-Deno.cron("json", { minute: { every: 2 } }, 
+Deno.cron("task2", { minute: { every: 2 } }, 
     () => console.log("This will print every TWO minutes"))
-
